406650a493e74903fdcc7d7b148ede0007f12e9d
-ffix <164929254+f-fix@users.noreply.github.com>
2026-02-09 14:17:24 -0800

    Add files via upload

--- mzm2met.py
+++ mzm2met.py
@@ -4,7 +4,7 @@ Extract original 8-bit console ROM image from MZM.
 Thanks to Liam for finding a bunch of the compressed data regions!
 Also Liam realized the formats were already known ones: SZS and LZSS.
 See: https://problemkaputt.de/gbatek-lz-decompression-functions.htm
-Example usage:
+Example usage: (`mzm.in` and `mzm-eur.in`)
 ```bash
 python3 mzm2met.py \
     mzm.in \
@@ -36,11 +36,45 @@ python3 mzm2met.py \
     `+0x07f6b94:0x1b4c0-0x1b8bf \
     met.out
 ```
-MZM this was developed for:
+Example usage: (`mzm-jp.in`)
+```bash
+python3 mzm2met.py \
+    mzm-jp.in \
+      0x07dd1c0:0x00000-0x1ffff` : \
+    `+0x07f6ff8:0x04d60-0x0515f` : \
+    `+0x07f4a88:0x05160-0x0555f` : \
+    `+0x07f47a0:0x08d60-0x0915f` : \
+    `+0x07f54ac:0x09160-0x0955f` : \
+    `+0x07f4de8:0x0cd60-0x0d15f` : \
+    `+0x07f5178:0x0d160-0x0d55f` : \
+    `+0x07f44a8:0x10d60-0x1127f` : \
+    `+0x07f62c8:0x11360-0x1155f` : \
+    `+0x07f69c0:0x14d60-0x151af` : \
+    `+0x07f3c68:0x18000-0x1899f` : \
+    `+0x07f75a4:0x189a0-0x18a9f` : \
+    `+0x07f7288:0x18be0-0x190df` : \
+    `+0x07f365c:0x190e0-0x1988f` : \
+    `+0x07f7278:0x19890-0x1989f` : \
+    `+0x07f77a4:0x19980-0x199bf` : \
+    `+0x07f7658:0x19da0-0x19eef` : \
+    `+0x07f5cec:0x19ef0-0x1a6ef` : \
+    `+0x07f6780:0x1a6f0-0x1a94f` : \
+    `+0x07f6d78:0x1a950-0x1a9bf` : \
+    `+0x07f64c8:0x1a9c0-0x1ac9f` : \
+    `+0x07f57d4:0x1aca0-0x1b29f` : \
+    `+0x07f5c5c:0x1b2a0-0x1b32f` : \
+    `+0x07f6414:0x1b330-0x1b3ef` : \
+    `+0x07f6208:0x1b3f0-0x1b4af` : \
+    `+0x07f6df4:0x1b4c0-0x1b8bf \
+    met-jp.out
+```
+MZM versions this was developed for:
 ```
 8.0M mzm.in crc32:5c61a844 md5:ebbce58109988b6da61ebb06c7a432d5 sha1:5de8536afe1f0078ee6fe1089f890e8c7aa0a6e8 sha256:fc94f65380b65b870a30b9b04b39cca1dc63d6e46a4a373d3904adc0912ebc37 size:8388608
 
 8.0M mzm-eur.in crc32:f1d92e63 md5:07930e72d4824bd63827a1a823cc8829 sha1:0fd107445a42e6f3a3e5ce8c865f412583179903 sha256:0d061ff36c62ebbf2220e106ea34abdb8d813943515b35007fd8fdba3ed019b0 size:8388608
+
+8.0M mzm-jp.in crc32:44b79e2b md5:650140ccae6e1ab7af44c74986002375 sha1:096f07685a3dc9286e71aa0b761f233b5efa2fcd sha256:d087ca6e2d1808109a579c2283e944d81c8426d37de564f99ea9273087c2172a size:8388608
 ```
 """
 
